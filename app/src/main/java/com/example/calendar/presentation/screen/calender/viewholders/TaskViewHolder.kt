c56410cb1d4e96df460a18a88a519f54048d233a
yogeshMarkandey <yogeshmarkandey1234@gmail.com>
2022-07-31 23:10:16 +0530

    fix: task card ui

--- app/src/main/java/com/example/calendar/presentation/screen/calender/viewholders/TaskViewHolder.kt
+++ app/src/main/java/com/example/calendar/presentation/screen/calender/viewholders/TaskViewHolder.kt
@@ -2,10 +2,14 @@ package com.example.calendar.presentation.screen.calender.viewholders
 
 import android.view.View
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.calendar.R
 import com.example.calendar.domain.models.Task
 import com.example.calendar.presentation.screen.calender.adapters.TaskRVAdapter
+import com.example.calendar.presentation.util.ColorSchemeType
+import com.example.calendar.presentation.util.ViewHelper
+import com.example.calendar.presentation.util.ViewHelper.getColorsFromResource
 
 class TaskViewHolder(
     itemView: View,
@@ -14,12 +18,48 @@ class TaskViewHolder(
 
     private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
     private val tvDescription = itemView.findViewById<TextView>(R.id.tvDescription)
+    private val sideView = itemView.findViewById<View>(R.id.sideView)
+    private val rootLayout = itemView.findViewById<CardView>(R.id.rootCardView)
 
     fun bind(task: Task) {
+        setupColorScheme(getColorScheme(taskId = task.id))
         itemView.setOnClickListener {
             onTaskCardClicked.onDeleteClicked(task)
         }
         tvTitle?.text = task.title
         tvDescription?.text = task.description
+    }
+
+    private fun getColorScheme(taskId: Int) : ColorSchemeType{
+       return when(taskId%3){
+            0 -> ColorSchemeType.Yellow
+            1 -> ColorSchemeType.Green
+            2 -> ColorSchemeType.Blue
+            else -> ColorSchemeType.Yellow
+        }
+    }
+
+    private fun setupColorScheme(type: ColorSchemeType){
+        when(type){
+            ColorSchemeType.Yellow -> {
+                rootLayout.setCardBackgroundColor(getColorsFromResource(itemView.context, R.color.yellow_light))
+                sideView.setBackgroundColor(getColorsFromResource(itemView.context, R.color.yellow))
+                tvTitle.setTextColor(getColorsFromResource(itemView.context, R.color.yellow))
+                tvDescription.setTextColor(getColorsFromResource(itemView.context, R.color.yellow_medium))
+            }
+            ColorSchemeType.Green -> {
+                rootLayout.setBackgroundColor(getColorsFromResource(itemView.context, R.color.green_light))
+                sideView.setBackgroundColor(getColorsFromResource(itemView.context, R.color.green))
+                tvTitle.setTextColor(getColorsFromResource(itemView.context, R.color.green))
+                tvDescription.setTextColor(getColorsFromResource(itemView.context, R.color.green_medium))
+            }
+            ColorSchemeType.Blue -> {
+                rootLayout.setBackgroundColor(getColorsFromResource(itemView.context, R.color.blue_light))
+                sideView.setBackgroundColor(getColorsFromResource(itemView.context, R.color.blue))
+                tvTitle.setTextColor(getColorsFromResource(itemView.context, R.color.blue))
+                tvDescription.setTextColor(getColorsFromResource(itemView.context, R.color.blue_medium))
+            }
+        }
+        rootLayout.radius = ViewHelper.getRadius(itemView.context, 8F)
     }
 }
