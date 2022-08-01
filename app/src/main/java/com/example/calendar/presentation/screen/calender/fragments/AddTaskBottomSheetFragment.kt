9bad9890e4b7f09b57d842055c84171b516fe226
yogeshMarkandey <yogeshmarkandey1234@gmail.com>
2022-08-01 08:54:27 +0530

    fix:ui

--- app/src/main/java/com/example/calendar/presentation/screen/calender/fragments/AddTaskBottomSheetFragment.kt
+++ app/src/main/java/com/example/calendar/presentation/screen/calender/fragments/AddTaskBottomSheetFragment.kt
@@ -29,9 +29,8 @@ class AddTaskBottomSheetFragment(
         val button = contentView.findViewById<Button>(R.id.addTaskButton)
         val evTitle = contentView.findViewById<EditText>(R.id.etTitle)
         val evDescription = contentView.findViewById<EditText>(R.id.etDescription)
-        val etTags = contentView.findViewById<EditText>(R.id.etTags)
         button.setOnClickListener {
-            onAddClickedListener.onClick(evTitle?.text.toString(), evDescription?.text.toString(), etTags?.text.toString())
+            onAddClickedListener.onClick(evTitle?.text.toString(), evDescription?.text.toString(), "")
             dialog?.cancel()
         }
         return contentView
